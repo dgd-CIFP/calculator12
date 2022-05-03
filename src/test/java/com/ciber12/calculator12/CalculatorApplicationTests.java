4d506b50593f77738a286a211c1592fb8ce18d28
ciber12 <dgd62340@educastur.es>
2022-05-03 17:55:22 +0200

    Se añaden suma, controlador y test unitario

---
+++ src/test/java/com/ciber12/calculator12/CalculatorApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ciber12.calculator12;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+public class CalculatorApplicationTests {
+
+	private Calculator calculator = new Calculator();
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void testSum() {
+		assertEquals(5, calculator.sum(2, 3));
+	}
+}
