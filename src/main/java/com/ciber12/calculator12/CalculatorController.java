6d5a8e92c99c46e7e17d707d8a23c69b812c4c68
ciber12 <dgd62340@educastur.es>
2022-05-06 19:39:54 +0200

    definitiva?

--- src/main/java/com/ciber12/calculator12/CalculatorController.java
+++ src/main/java/com/ciber12/calculator12/CalculatorController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class CalculatorController {
-@Autowired
-private Calculator calculator;
+	@Autowired
+	private Calculator calculator;
 
-@RequestMapping("/sum")
-String sum(@RequestParam("a") Integer a,
-@RequestParam("b") Integer b) {
-return String.valueOf(calculator.sum(a, b));
-}
+	@RequestMapping("/sum")
+	String sum(@RequestParam("a") Integer a,
+			@RequestParam("b") Integer b) {
+		return String.valueOf(calculator.sum(a, b));
+	}
 }
